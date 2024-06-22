8fa805c53f68d139af31387030a462a5a1f0b615
ish <ish@ish.works>
2024-06-22 19:09:31 +0200

    Initial Commit

---
+++ mastodon-archive-tool/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mastodon-archive-tool
+//
+//  Created by Wolfe on 22.06.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mastodon-archive-tool/mastodon_archive_toolApp.swift
@@ -0,0 +1,17 @@
+//
+//  mastodon_archive_toolApp.swift
+//  mastodon-archive-tool
+//
+//  Created by Wolfe on 22.06.24.
+//
+
+import SwiftUI
+
+@main
+struct mastodon_archive_toolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
